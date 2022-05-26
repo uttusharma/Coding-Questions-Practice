6796d78f00e9e2d13e2ed762be5502566efd4d84
Utkarsh Sharma <utkarshstudent1@gmail.com>
2022-05-26 17:33:23 +0530

    Time: 12 ms (7.39%), Space: 8.5 MB (17.32%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -12,19 +12,39 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res;
-        stack<TreeNode*> st;
-        TreeNode* curr = root;
-        while(curr != NULL || !st.empty()){
-            while(curr != NULL){
-                st.push(curr);
-                curr = curr->left;
+        stack <TreeNode*> st;
+        TreeNode* node = root; 
+        vector<int> a;
+        
+        while(true){
+            if(node != NULL){
+                st.push(node); // we push the root node into the stack
+                node = node -> left; // after that we push the consecutive leftward nodes into the stack
             }
-            curr = st.top();
-            st.pop();
-            res.push_back(curr->val);
-            curr = curr->right;
+            else {
+                if(st.empty() == true) break;
+                
+                node = st.top(); // we assign the topmost left node to node
+                st.pop(); 
+                
+                a.push_back(node->val); // we start pushing
+                node = node -> right; // after the stack is empty then we move to the rightward side
+            }
         }
-        return res;
+    return a;
+        // vector<int> res;
+        // stack<TreeNode*> st;
+        // TreeNode* curr = root;
+        // while(curr != NULL || !st.empty()){
+        //     while(curr != NULL){
+        //         st.push(curr);
+        //         curr = curr->left;
+        //     }
+        //     curr = st.top();
+        //     st.pop();
+        //     res.push_back(curr->val);
+        //     curr = curr->right;
+        // }
+        // return res;
     }
 };
