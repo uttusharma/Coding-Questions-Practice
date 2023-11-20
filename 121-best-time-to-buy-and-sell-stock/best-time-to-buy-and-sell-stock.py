e2585e69ce2581eb5a9ec81fcdc69ae43a47f9da
Utkarsh Sharma <utkarshstudent1@gmail.com>
2023-11-20 22:27:05 +0530

    Time: 833 ms (74.51%) | Memory: 27.3 MB (66.27%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -6,11 +6,11 @@ class Solution:
         buy_price = prices[0] # at the begining the minimum price is the first price
         profit = 0 # at the begining the minimum  profit is zero
         
-        for i in range(1,len(prices)):
+        for curr in prices:
             #if the current price is less than the previous buy price ; update the buy_price
-            if prices[i] < buy_price:
-                buy_price = prices[i]
+            if curr < buy_price:
+                buy_price = curr
             else: # if not check if you sell with the current price would you get better profit than the previous one
-                profit = max(profit, prices[i]-buy_price) # compare the previous profit with the current profit
+                profit = max(profit, curr-buy_price) # compare the previous profit with the current profit
                 
         return profit
