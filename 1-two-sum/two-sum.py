93aa9e1dd29d12d5b0eaec817b82574cf390dde8
Utkarsh Sharma <utkarshstudent1@gmail.com>
2023-12-09 15:21:21 +0530

    Time: 75 ms (43.03%) | Memory: 17.6 MB (42.86%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,28 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums = sorted(enumerate(nums), key=lambda x: x[1])
-        left = 0
-        right = len(nums) - 1
-        while left < right:
-            sum = nums[left][1] + nums[right][1]
-            if sum == target:
-                return [nums[left][0], nums[right][0]]
-            elif sum < target:
-                left += 1
-            else:
-                right -= 1
+        map = {}
+
+        for i in range(len(nums)):
+           map[nums[i]] = i
+        
+        for i in range(len(nums)):
+            diff =  target - nums[i]
+            if diff in map and map[diff] != i:
+                return [map[diff], i]
+
         return []
-
-
-        # -- ONE PASS HASHMAP -- 
-        # seen = {}
-        
-        # for i in range(len(nums)):
-        #     diff = target - nums[i]
-
-        #     if diff in seen:
-        #         return [seen[diff], i]
-        #     else:
-        #         seen[nums[i]] = i
-        
-        # return []
